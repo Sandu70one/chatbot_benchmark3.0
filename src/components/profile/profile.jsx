808fbbd590cec2563916e8b95271ca67f4f4f635
Sandu70one <sanduaruna70one@gmail.com>
2024-12-07 09:14:19 +0530

    Add profile component styles and enhance input fields

--- src/components/profile/profile.jsx
+++ src/components/profile/profile.jsx
@@ -11,7 +11,7 @@ import {
 import profileImg from "../../assets/profilePic.png";
 
 function profile() {
-  const [activeSection, setActiveSection] = React.useState(null);
+  const [activeSection, setActiveSection] = React.useState(0);
 
   const handleSectionClick = (section) => {
     setActiveSection(section);
@@ -32,7 +32,7 @@ function profile() {
       </div>
       <hr className="border-t-2 border-[var(--secondary-hr-clr)]" />
 
-      <div className="grid grid-cols-12  w-full">
+      <div className="grid grid-cols-12  w-full ">
         <div className="col-span-4 flex justify-between">
           <div className="flex flex-col my-6 w-full">
             {[
@@ -46,7 +46,7 @@ function profile() {
                 key={index}
                 className={`py-4 flex justify-center items-center w-full gap-4 cursor-pointer ${
                   activeSection === index
-                    ? "bg-[var(--profile-btn-bg)]"
+                    ? "bg-[var(--profile-btn-bg)] button-selected-color"
                     : "hover:bg-[var(--profile-btn-bg)]"
                 }`}
                 onClick={() => handleSectionClick(index)}
@@ -58,14 +58,58 @@ function profile() {
           </div>
           <div className="w-0.5 h-auto bg-[var(--secondary-hr-clr)]"></div>
         </div>
-        <div className="col-span-8 mt-6">
-          <div className="flex flex-col ml-9">
+        <div className="col-span-8 mt-6 h-100 ">
+          <div className="flex flex-col ml-9 m-6 gap-4 ">
             <div>Profile Picture</div>
             <div>
-              <img src={profileImg} alt="profile image" />
+              <img
+                src={profileImg}
+                alt="profile image "
+                className="hover:opacity-30 "
+              />
             </div>
-            <div>
-              Profile name
+            <div className="pt-10 text-[var(--grey-4)]">Profile name</div>
+            <input
+              type="text"
+              placeholder="Naleeka Kumarasinghe"
+              id="exampleInput"
+              className="w-96 p-4 border border-[var(--secondary-hr-clr)] rounded-lg bg-transparent focus:outline-none focus:ring-1 focus:ring-white focus:border-white placeholder-white"
+            />
+            <div className="text-[var(--grey-4)]">Email address</div>
+            <input
+              type="email"
+              placeholder="naleeka123@gmail.com"
+              id="exampleInput"
+              class="w-96 p-4 border border-[var(--secondary-hr-clr)] text-[var(--grey-4)]  rounded-lg bg-transparent focus:outline-none focus:ring-1 focus:ring-[var(--secondary-hr-clr)] focus:border-[var(--secondary-hr-clr)]"
+              disabled
+            />
+            <div class="w-full max-w-sm mx-start">
+              <label
+                for="dropdown"
+                class="block text-sm font-medium text-[var(--grey-4)] mb-2"
+              >
+                Language preference
+              </label>
+              <select
+                id="dropdown"
+                class="w-full p-3  border border-[var(--secondary-hr-clr)] rounded-lg bg-transparent focus:outline-none focus:ring-2 focus:ring-[var(--secondary-hr-clr)] focus:border-[var(--secondary-hr-clr)] "
+              >
+                <option value="" disabled selected>
+                  Auto detect
+                </option>
+                <option value="option1 bg-black">Option 1</option>
+                <option value="option2">Option 2</option>
+                <option value="option3">Option 3</option>
+              </select>
+            </div>
+
+            <div className="flex justify-end">
+              <button className="bg-[var(--text-field-bg-blocked)] rounded-lg py-2 px-4 mr-2">
+                Cancel
+              </button>
+              <button className="text-white button-bg-color rounded-lg py-2 px-4 ">
+                Save
+              </button>
             </div>
           </div>
         </div>
